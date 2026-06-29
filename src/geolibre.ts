be62b78902f8e89499d0012fa8266f3a77e3544a
Qiusheng Wu <giswqs@gmail.com>
2026-06-29 08:13:52 -0400

    Bump GeoLibre plugin manifest to 0.2.2

--- src/geolibre.ts
+++ src/geolibre.ts
@@ -111,7 +111,7 @@ function isNasaOperaProjectState(
 export const plugin: GeoLibrePlugin<GeoLibreControl> = {
   id: "geolibre-nasa-opera",
   name: "NASA OPERA",
-  version: "0.2.1",
+  version: "0.2.2",
   activate(app) {
     operaControl = operaControl ?? createControl(app);
     const added = app.addMapControl(operaControl, position);
